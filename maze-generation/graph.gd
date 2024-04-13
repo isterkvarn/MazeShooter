19c50b682366038c60635a9f2975578694abb8f0
Dethcrvsh <fabianfabbe@hotmail.se>
2024-04-13 12:50:19 +0200

    we generate a maze now

--- maze-generation/graph.gd
+++ maze-generation/graph.gd
@@ -11,8 +11,6 @@ func _init(width_in: int, height_in: int):
 	height = height_in
 	_init_connections(width, height)
 
-	print(connections)
-
 func _init_connections(width, height):
 	connections = []
 	for y in range(height):
@@ -24,8 +22,9 @@ func add_connection(node1: Vector2i, node2: Vector2i):
 	connections[node1.y][node1.x].append(node2)
 	connections[node2.y][node2.x].append(node1)
 
+func has_connection(node1: Vector2i, node2: Vector2i):
+	return node1 in connections[node2.y][node2.x]
+
 func get_connections(node: Vector2i):
 	return connections[node.y][node.x]
 
-func print_graph():
-	print(connections)
