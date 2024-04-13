45e5608da387d26e5621a595ce182b04a041826b
eliasjohansson <eljoh633@student.liu.se>
2024-04-13 14:53:47 +0200

    goat man finished

--- goat_man/goat_man.gd
+++ goat_man/goat_man.gd
@@ -1,15 +1,14 @@
 extends Node3D
 
-const SPEED = 10
+const SPEED = 30
 const CORRIDOR_SIZE = 5
+const POS_MARGIN = 0.2
 var forward = Vector3.FORWARD
 var turn_timer = 0
 
 @onready var right_ray = $RayCastRight
 @onready var left_ray  = $RayCastLeft
 @onready var forward_ray = $RayCastForward
-@onready var turn_left_ray = $RayCastForwardLeft
-@onready var turn_right_ray = $RayCastForwardRight
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,18 +19,21 @@ func _process(delta):
 	# Always run forward
 	global_position += forward*SPEED*delta
 
-	print((not left_ray.is_colliding()) and turn_left_ray.is_colliding())
+	#print(in_grid())
 	
-	if (forward_ray.is_colliding() 
-	or ((not left_ray.is_colliding()) and turn_left_ray.is_colliding()) 
-	or ((not right_ray.is_colliding()) and turn_right_ray.is_colliding())) and turn_timer <= 0:
+	if forward_ray.is_colliding() or (in_grid() and turn_timer <= 0 and 
+	(not left_ray.is_colliding() or not right_ray.is_colliding())):
 		
 		turn_timer = 0.2
 		
 		# Adjust so in middle of corridor
-		#global_position -= forward * (CORRIDOR_SIZE - forward_distance)
+		global_position.x = snapped(global_position.x, 5)
+		global_position.y = snapped(global_position.y, 5)
 		
 		var turn_options = []
+		
+		if not forward_ray.is_colliding():
+			turn_options.append(0)
 		
 		if not right_ray.is_colliding():
 			turn_options.append(PI/2)
@@ -39,7 +41,7 @@ func _process(delta):
 		if not left_ray.is_colliding():
 			turn_options.append(-PI/2)
 			
-		var turn
+		var turn = 0
 		
 		if turn_options:
 			turn = turn_options.pick_random()
@@ -48,3 +50,15 @@ func _process(delta):
 			
 		rotate_y(turn)
 		forward = forward.rotated(Vector3(0, 1, 0), turn)
+		
+func in_grid():
+	var global_pos = abs(global_position)
+	var decimal_x = global_pos.x - int(global_pos.x)
+	var decimal_z = global_pos.z - int(global_pos.z)
+	var mod_x = int(global_pos.x) % 5
+	var mod_z = int(global_pos.z) % 5
+	
+	var within_x = (POS_MARGIN > (mod_x + decimal_x)) or (POS_MARGIN > (mod_x - decimal_x))
+	var within_z = (POS_MARGIN > (mod_z + decimal_z)) or (POS_MARGIN > (mod_z - decimal_z))
+	
+	return within_x and within_z
