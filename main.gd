3322a346f0af3ef2891625b81e66a3ecdca52edb
Dethcrvsh <fabianfabbe@hotmail.se>
2024-04-13 17:21:29 +0200

    did maze stuff

--- main.gd
+++ main.gd
@@ -2,13 +2,18 @@ extends Node3D
 
 @onready var maze = $Maze
 @onready var player = %Player
+@onready var goat_res = preload("res://goat_man/goat_man.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	maze.create(4, 4)
-	var spawn_pos = maze.get_spawn_coords()
-	player.transform.origin = Vector3(spawn_pos.x, 6, spawn_pos.y)
-
+	maze.create(2, 2)
+	player.transform.origin = maze.get_spawn_coords()
+	
+	# Spawn a testing goat
+	var goat_pos = maze.get_random_pos()
+	var goat = goat_res.instantiate()
+	goat.transform.origin = maze.get_random_pos()
+	add_child(goat)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
