e9a9e29a4bc7503285eb47ec4c1bb1096abf627d
eliasjohansson <eljoh633@student.liu.se>
2024-04-13 10:42:32 +0200

    added gun viewmodel and muzzle flash

--- player/player.gd
+++ player/player.gd
@@ -9,6 +9,9 @@ const MOUSE_SENSITIVITY = 0.003
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var rotation_helper = $RotationHelper
+@onready var muzzle = $RotationHelper/MuzzleFlash
+var muzzle_timer = 0
+const MUZZLE_TIME = 0.2
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -24,6 +27,13 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	handle_shoot(delta)
+	
+	if muzzle_timer > 0:
+		muzzle_timer -= delta
+	else:
+		muzzle.visible = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -43,3 +53,15 @@ func _input(event):
 		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
 		rotation_helper.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
 		rotation_helper.rotation.x = clampf(rotation_helper.rotation.x, -PI/2, PI/2)
+		
+func handle_shoot(delta):
+	
+	# Handle muzzle flash
+	if Input.is_action_just_pressed("shoot"):
+		muzzle.visible = true
+		muzzle_timer = MUZZLE_TIME
+	
+	if muzzle_timer > 0:
+		muzzle_timer -= delta
+	else:
+		muzzle.visible = false
